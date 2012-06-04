898d997466d608ca92317ec8f5f0961758c0e848
Moiz Merchant <moiz@tiktok.com>
2012-06-04 14:20:32 -0600

    Changes to make into library and background networking.

--- src/com/ijsbrandslob/appirater/Appirater.java
+++ src/com/ijsbrandslob/appirater/Appirater.java
@@ -1,9 +1,9 @@
 /*
  This file is part of Appirater.
- 
+
  Copyright (c) 2010, Arash Payan
  All rights reserved.
- 
+
  Permission is hereby granted, free of charge, to any person
  obtaining a copy of this software and associated documentation
  files (the "Software"), to deal in the Software without
@@ -12,10 +12,10 @@
  copies of the Software, and to permit persons to whom the
  Software is furnished to do so, subject to the following
  conditions:
- 
+
  The above copyright notice and this permission notice shall be
  included in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -28,34 +28,35 @@
 /*
  * Appirater.java
  * Port of Appirater to Android.
- * 
+ *
  * Original created by Arash Payan on 9/5/09.
  * http://arashpayan.com
  * Copyright 2010 Arash Payan. All rights reserved.
- * 
+ *
  * Ported by IJsbrand Slob on 3/7/11.
  * http://ijsbrandslob.com
  */
 
 package com.ijsbrandslob.appirater;
 
-import java.net.URI;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import com.mirast.test.Appirater.TestProject.R;
-
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -66,14 +67,14 @@ public class Appirater {
     * Users will need to have the same version of your app installed for this
     * many days before they will be prompted to rate it.
     */
-   private static final int DAYS_UNTIL_PROMPT = 30;
+   private static final int DAYS_UNTIL_PROMPT = 15;
 
    /*
     * An example of a 'use' would be if the user launched the app. Bringing the
     * app into the foreground (on devices that support it) would also be
     * considered a 'use'. You tell Appirater about these events using the two
     * methods: Appirater.appLaunched(); Appirater.appEnteredForeground();
-    * 
+    *
     * Users need to 'use' the same version of the app this many times before
     * before they will be prompted to rate it.
     */
@@ -106,6 +107,7 @@ public class Appirater {
    private static final boolean DEBUG = false;
 
    private Context mContext;
+   private Handler mHandler;
    private Date mFirstUseDate;
    private Date mReminderRequestDate;
    private int mUseCount;
@@ -113,17 +115,18 @@ public class Appirater {
    private int mCurrentVersion;
    private boolean mRatedCurrentVersion;
    private boolean mDeclinedToRate;
-   
-   public Appirater( Context context ) {
+
+   public Appirater( Context context, Handler handler ) {
       mContext = context;
+      mHandler = handler;
       loadSettings();
    }
-   
+
    /*
     * Tells Appirater that the app has launched. You should call this method at
     * the end of your application main activity delegate's Activity.onStart()
     * method.
-    * 
+    *
     * If the app has been used enough to be rated (and enough significant
     * events), you can suppress the rating alert by passing false for
     * canPromptForRating. The rating alert will simply be postponed until it is
@@ -131,15 +134,19 @@ public class Appirater {
     * be triggered by appEnteredForeground() and userDidSignificantEvent() (as
     * long as you pass true for canPromptForRating in those methods).
     */
-   public void appLaunched( boolean canPromptForRating ) {
-      incrementAndRate( canPromptForRating );
+   public void appLaunched( final boolean canPromptForRating ) {
+      new Thread(new Runnable() {
+          public void run() {
+              incrementAndRate( canPromptForRating );
+          }
+      }).start();
    }
 
    /*
     * Tells Appirater that the app was brought to the foreground on multitasking
     * devices. You should call this method from the application delegate's
     * Activity.onResume() method.
-    * 
+    *
     * If the app has been used enough to be rated (and enough significant
     * events), you can suppress the rating alert by passing false for
     * canPromptForRating. The rating alert will simply be postponed until it is
@@ -147,8 +154,12 @@ public class Appirater {
     * be triggered by appLaunched() and userDidSignificantEvent() (as long as
     * you pass true for canPromptForRating in those methods).
     */
-   public void appEnteredForeground( boolean canPromptForRating ) {
-      incrementAndRate( canPromptForRating );
+   public void appEnteredForeground( final boolean canPromptForRating ) {
+      new Thread(new Runnable() {
+          public void run() {
+              incrementAndRate( canPromptForRating );
+          }
+      }).start();
    }
 
    /*
@@ -157,7 +168,7 @@ public class Appirater {
     * calls, then you might want to call this method whenever the user places a
     * call. If it's a game, you might want to call this whenever the user beats
     * a level boss.
-    * 
+    *
     * If the user has performed enough significant events and used the app
     * enough, you can suppress the rating alert by passing false for
     * canPromptForRating. The rating alert will simply be postponed until it is
@@ -168,23 +179,31 @@ public class Appirater {
    public void userDidSignificantEvent( boolean canPromptForRating ) {
       incrementSignificantEventAndRate( canPromptForRating );
    }
-   
+
    private void incrementAndRate( boolean canPromptForRating ) {
       incrementUseCount();
       if (canPromptForRating && ratingConditionsHaveBeenMet()
             && connectedToNetwork()) {
-         showRatingAlert();
+         mHandler.post(new Runnable() {
+             public void run() {
+                 showRatingAlert();
+             }
+         });
       }
    }
-   
+
    private void incrementSignificantEventAndRate( boolean canPromptForRating ) {
       incrementSignificantEventCount();
       if (canPromptForRating && ratingConditionsHaveBeenMet()
             && connectedToNetwork()) {
-         showRatingAlert();
+         mHandler.post(new Runnable() {
+             public void run() {
+                 showRatingAlert();
+             }
+         });
       }
    }
-   
+
    private boolean connectedToNetwork() {
       try {
          HttpClient httpclient = new DefaultHttpClient();
@@ -195,14 +214,15 @@ public class Appirater {
             return true;
          }
       } catch( Exception ex ) {
+         Log.w("Appirater", ex.toString());
       }
       return false;
    }
-   
+
    private void showRatingAlert() {
       final Dialog rateDialog = new Dialog( mContext );
       final Resources res = mContext.getResources();
-      
+
       CharSequence appname = "unknown";
       try {
          appname = mContext.getPackageManager().getApplicationLabel( mContext.getPackageManager().getApplicationInfo( mContext.getPackageName(), 0 ) );
@@ -210,28 +230,38 @@ public class Appirater {
       }
       rateDialog.setTitle( String.format( res.getString( R.string.APPIRATER_MESSAGE_TITLE ), appname ) );
       rateDialog.setContentView( R.layout.appirater );
-      
+
       TextView messageArea = (TextView)rateDialog.findViewById( R.id.appirater_message_area );
       messageArea.setText( String.format( res.getString( R.string.APPIRATER_MESSAGE ), appname ) );
-      
+
       Button rateButton = (Button)rateDialog.findViewById( R.id.appirater_rate_button );
       rateButton.setText( String.format( res.getString( R.string.APPIRATER_RATE_BUTTON ), appname ) );
       Button remindLaterButton = (Button)rateDialog.findViewById( R.id.appirater_rate_later_button );
       Button cancelButton = (Button)rateDialog.findViewById( R.id.appirater_cancel_button );
-      
+
       rateButton.setOnClickListener( new OnClickListener() {
          @Override
          public void onClick( View v ) {
+            rateDialog.dismiss();
+
+            PackageManager packageManager = mContext.getPackageManager();
             Uri marketUri = Uri.parse( String.format( "market://details?id=%s", mContext.getPackageName() ) );
             Intent marketIntent = new Intent( Intent.ACTION_VIEW ).setData( marketUri );
-            mContext.startActivity( marketIntent );
+
+            List<?> list = packageManager.queryIntentActivities(marketIntent, PackageManager.MATCH_DEFAULT_ONLY);
+            if (list.size() > 0) {
+                mContext.startActivity( marketIntent );
+            } else {
+                Uri webUri = Uri.parse( String.format( "http://play.google.com/store/apps/details?id=%s", mContext.getPackageName() ) );
+                Intent webIntent = new Intent( Intent.ACTION_VIEW ).setData( webUri );
+                mContext.startActivity( webIntent );
+            }
+
             mRatedCurrentVersion = true;
             saveSettings();
-            
-            rateDialog.dismiss();
          }
       });
-      
+
       remindLaterButton.setOnClickListener( new OnClickListener() {
          @Override
          public void onClick( View v ) {
@@ -240,7 +270,7 @@ public class Appirater {
             rateDialog.dismiss();
          }
       });
-      
+
       cancelButton.setOnClickListener( new OnClickListener() {
          @Override
          public void onClick( View v ) {
@@ -256,34 +286,34 @@ public class Appirater {
       if( DEBUG ) {
          return true;
       }
-      
+
       Date now = new Date();
       long timeSinceFirstLaunch = now.getTime() - mFirstUseDate.getTime();
       long timeUntilRate = 1000 * 60 * 60 * 24 * DAYS_UNTIL_PROMPT;
       if( timeSinceFirstLaunch < timeUntilRate ) {
          return false;
       }
-      
+
       // check if the app has been used enough
       if( mUseCount < USES_UNTIL_PROMPT ) {
          return false;
       }
-      
+
       // check if the user has done enough significant events
       if( mSignificantEventCount < SIG_EVENTS_UNTIL_PROMPT ) {
          return false;
       }
-      
+
       // has the user previously declined to rate this version of the app?
       if( mDeclinedToRate ) {
          return false;
       }
-      
+
       // has the user already rated the app?
       if( mRatedCurrentVersion ) {
          return false;
       }
-      
+
       // if the user wanted to be reminded later, has enough time passed?
       if( null != mReminderRequestDate ) {
          long timeSinceReminderRequest = mReminderRequestDate.getTime() - now.getTime();
@@ -294,7 +324,7 @@ public class Appirater {
       }
       return true;
    }
-   
+
    private void incrementUseCount() {
       // get the app's version
       int version = -1;
@@ -302,25 +332,25 @@ public class Appirater {
          version = mContext.getPackageManager().getPackageInfo( mContext.getPackageName(), 0 ).versionCode;
       } catch( NameNotFoundException ex ){
       }
-      
+
       // get the version number that we've been tracking
       if( mCurrentVersion == -1 ) {
          mCurrentVersion = version;
       }
-      
+
       if( DEBUG ) {
          System.out.println( String.format( "APPIRATER Tracking version: %d", mCurrentVersion ) );
       }
-      
+
       if( mCurrentVersion == version ) {
          // check if the first use date has been set. if not, set it.
          if( mFirstUseDate == null ) {
             mFirstUseDate = new Date();
          }
-         
+
          // increment the use count
          ++mUseCount;
-         
+
          if( DEBUG ) {
             System.out.println( String.format( "APPIRATER Use count: %d", mUseCount ) );
          }
@@ -335,10 +365,10 @@ public class Appirater {
          mDeclinedToRate = false;
          mReminderRequestDate = null;
       }
-      
+
       saveSettings();
    }
-   
+
    private void incrementSignificantEventCount() {
       // get the app's version
       int version = -1;
@@ -346,25 +376,25 @@ public class Appirater {
          version = mContext.getPackageManager().getPackageInfo( mContext.getPackageName(), 0 ).versionCode;
       } catch( NameNotFoundException ex ){
       }
-      
+
       // get the version number that we've been tracking
       if( mCurrentVersion == -1 ) {
          mCurrentVersion = version;
       }
-      
+
       if( DEBUG ) {
          System.out.println( String.format( "APPIRATER Tracking version: %d", mCurrentVersion ) );
       }
-      
+
       if( mCurrentVersion == version ) {
          // check if the first use date has been set. if not, set it.
          if( mFirstUseDate == null ) {
             mFirstUseDate = new Date();
          }
-         
+
          // increment the significant event count
          ++mSignificantEventCount;
-         
+
          if( DEBUG ) {
             System.out.println( String.format( "APPIRATER Significant event count: %d", mSignificantEventCount ) );
          }
@@ -378,10 +408,10 @@ public class Appirater {
          mDeclinedToRate = false;
          mReminderRequestDate = null;
       }
-      
+
       saveSettings();
    }
-   
+
    // Settings
    private static final String APPIRATER_FIRST_USE_DATE = "APPIRATER_FIRST_USE_DATE";
    private static final String APPIRATER_REMINDER_REQUEST_DATE = "APPIRATER_REMINDER_REQUEST_DATE";
@@ -390,23 +420,23 @@ public class Appirater {
    private static final String APPIRATER_CURRENT_VERSION = "APPIRATER_CURRENT_VERSION";
    private static final String APPIRATER_RATED_CURRENT_VERSION = "APPIRATER_RATED_CURRENT_VERSION";
    private static final String APPIRATER_DECLINED_TO_RATE = "APPIRATER_DECLINED_TO_RATE";
-   
+
    private void loadSettings() {
-      Resources res = mContext.getResources();
+      //Resources res = mContext.getResources();
       SharedPreferences settings = mContext.getSharedPreferences( mContext.getPackageName(), Context.MODE_PRIVATE );
-      
+
       // Did we save settings before?
       if( settings.contains( APPIRATER_FIRST_USE_DATE ) ) {
          long firstUseDate = settings.getLong( APPIRATER_FIRST_USE_DATE, -1 );
          if( -1 != firstUseDate ) {
             mFirstUseDate = new Date( firstUseDate );
          }
-         
+
          long reminderRequestDate = settings.getLong( APPIRATER_REMINDER_REQUEST_DATE, -1 );
          if( -1 != reminderRequestDate ) {
             mReminderRequestDate = new Date( reminderRequestDate );
          }
-         
+
          mUseCount = settings.getInt( APPIRATER_USE_COUNT, 0 );
          mSignificantEventCount = settings.getInt( APPIRATER_SIG_EVENT_COUNT, 0 );
          mCurrentVersion = settings.getInt( APPIRATER_CURRENT_VERSION, 0 );
@@ -414,30 +444,30 @@ public class Appirater {
          mDeclinedToRate = settings.getBoolean( APPIRATER_DECLINED_TO_RATE, false );
       }
    }
-   
+
    private void saveSettings() {
-      Resources res = mContext.getResources();
+      //Resources res = mContext.getResources();
       SharedPreferences prefs = mContext.getSharedPreferences( mContext.getPackageName(), Context.MODE_PRIVATE );
       SharedPreferences.Editor editor = prefs.edit();
-      
+
       long firstUseDate = -1;
       if( mFirstUseDate != null ) {
          firstUseDate = mFirstUseDate.getTime();
       }
       editor.putLong( APPIRATER_FIRST_USE_DATE, firstUseDate );
-      
+
       long reminderRequestDate = -1;
       if( mReminderRequestDate != null ) {
          reminderRequestDate = mReminderRequestDate.getTime();
       }
       editor.putLong( APPIRATER_REMINDER_REQUEST_DATE, reminderRequestDate );
-      
+
       editor.putInt( APPIRATER_USE_COUNT, mUseCount );
       editor.putInt( APPIRATER_SIG_EVENT_COUNT, mSignificantEventCount );
       editor.putInt( APPIRATER_CURRENT_VERSION, mCurrentVersion );
       editor.putBoolean( APPIRATER_RATED_CURRENT_VERSION, mRatedCurrentVersion );
       editor.putBoolean( APPIRATER_DECLINED_TO_RATE, mDeclinedToRate );
-      
+
       editor.commit();
    }
 }
